fe6d85e19cef35c95eadc7c59a872a5a21cfd3ec
iceboy95 <hqkhor95@gmail.com>
2016-04-17 16:59:18 +0800

    latest changes

--- iceweb.m
+++ iceweb.m
@@ -1,4 +1,8 @@
-%%format: https://10.106.50.174/cgi/admin/getSDFile.cgi?file=20160210_181200.mp4&path=DCS-942L/20160210/18/20160210_181200.mp4
+%%vid url: https://10.106.50.174/cgi/admin/getSDFile.cgi?file=20160210_181200.mp4&path=DCS-942L/20160210/18/20160210_181200.mp4
+%%pic url: https://10.106.50.174/cgi/admin/getSDFile.cgi?file=20160210_181200.mp4&path=DCS-942L/20160210/18/20160210_181200.jpg
+
+%% (1) LOVIS cam: 10.106.50.174
+%% (2) LENA cam: 10.106.51.220
 
 %% Initial Variables
 startHour = 8;
@@ -6,12 +10,14 @@ totalHour = 18;
 hour = startHour;
 
 %% Automatic Downloader
-while hour < totalHour
-    %% URL credentials
-    format = '.mp4';
-    date = '20160215';
+while hour <= totalHour
+    %% URL variables
+    format = {'mp4','jpg'};
+    date = '20160412';
+    camIP = {'10.106.50.174','10.106.51.220'};
+    c = 1;          % camera index: 1 for LOVIS, 2 for LENA
     
-    
+    %% Time format manipulation
     minutes = 0;
     minutesEnd = 54; % For most case, except from 18(6pm) and 08(8am)
     if hour == 8
@@ -26,38 +32,35 @@ while hour < totalHour
     if hour < 10
         hourStr = strcat('0',hourStr);
     end
-    
     hourFolder = strcat('/',hourStr,'/');
     
-    while(minutes ~= minutesEnd)
+    while(minutes <= minutesEnd)
         % Concatenations for single digit situations
         minutesStr = int2str(minutes);
         if minutes < 10
             minutesStrCat = strcat('0',minutesStr);
             minutesStr = minutesStrCat;
-        end
-     
-        
-
+        end     
         time = strcat(hourStr,minutesStr ,'00');
-        %% URL concatenations
+        
+        %% for all formats available
+        for f = 1:length(format)
+            %% URL concatenations
+            baseUrl = ['https://',camIP{c},'/cgi/admin/getSDFile.cgi?file=',date,'_',time,'.',format{f},'&path=DCS-942L/'];
 
-        baseUrl = strcat( 'https://10.106.50.174/cgi/admin/getSDFile.cgi?file=',date,'_',time,format,'&path=DCS-942L/');
+            semifullPath = [date,hourFolder,date,'_',time,'.',format{f}];
+            semifullPath2 = ['https://',camIP{c},'/cgi/admin/getSDFile.cgi?file=',date,'_',time,'.',format{f},'&path=DCS-942L/'] ;
+            fullPath = strcat(semifullPath2,semifullPath);
 
-        semifullPath = strcat(date,hourFolder,date,'_',time,'.mp4');
-        semifullPath2 = ['https://10.106.50.174/cgi/admin/getSDFile.cgi?file=',date,'_',time,format,'&path=DCS-942L/'] ;
-        fullPath = strcat(semifullPath2,semifullPath);
-
-        %% Complete URL and showtime!  
-       % options = weboptions('Username','admin','Password','abc123');
-       % websave(semifullPath,fullPath,options);
+            %% Use browser to save
+            web(fullPath,'-browser');
+        end
+        
+        %% update time to next item, then pause
+        minutes = minutes + 6;          
        
-       web(fullPath,'-browser');
-        minutes = minutes + 6;
+        pause(35);   % pause here better, 20s enough per download
     end
     %% Counter Increment
-    pause(90);
     hour = hour + 1;
 end
-
-
